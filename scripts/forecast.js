d66371ae6c3fbe9d23d3323501006735423c9283
dthimmaiah <dthimmaiah@rdalabs.com>
2020-04-09 03:11:52 +0530

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,27 @@
+//const key = "JlNFsRJVOLDxfAWogZlnXbAFcQ12Bqc4";
+const key = "GG10F0JbsUuNcYtaCJwSTYk93RqM7oLo";
+//get weather information
+const getWeather = async (id) => {
+
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
